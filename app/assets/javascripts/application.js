d74dd1f97072fda5c6da98b58a38d3e7b99d4620
Wlodek Bzyl <matwb@univ.gda.pl>
2015-05-07 20:09:41 +0200

    update HTML Elelemnt Template example

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,19 +19,32 @@
 //= require_tree .
 
 $(function() {
+  console.log('try to attach ajax:success event to Show link');
+
   $('a[class^=show]').bind('ajax:success', function(event, data, status, xhr) {
-    $('body').append(JST["templates/show"]({
-      modal: 'fortune-modal',  // jakiś unikalny identyfikator
-      id: data.id,
-      quotation: data.quotation,
-      source: data.source }));
+    console.log('event ajax:success was fired');
+
+    var template = document.querySelector('#modal-show');
+
+    // get document fragment
+    var clone = template.content.cloneNode(true);
+    clone.querySelector('article').id = 'fortune-modal';
+
+    // populate document fragment at runtime
+    var p = clone.querySelectorAll('p');
+    p[0].textContent = data.quotation;
+    p[1].textContent = data.source;
+    var h3 = clone.querySelector('h3');
+    h3.textContent = 'Fortune ' + data.id;
+
+    // activate the template
+    $('body').prepend(clone);
+
+    // remove modal window from DOM
     $('#fortune-modal').on('hidden.bs.modal', function() {
-      $('.modal').remove();  // usuń wszystkie modals z DOM
+      $('.modal').remove();
     });
+
     $('#fortune-modal').modal('show');
   });
-
-  // $('a[class^=destroy]').bind('ajax:success', function(event, data, status, xhr) {
-  //   $(this).closest('article').effect('explode');
-  // });
 });
